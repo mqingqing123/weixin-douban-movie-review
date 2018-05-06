87df40533e683edc2643566f0516597f34956bc4
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2018-05-06 16:17:55 +0800

    create v2

--- app.js
+++ app.js
@@ -1,67 +1,39 @@
-'use strict';
-
-/**
- * 用于将微信官方`API`封装为`Promise`方式
- *  小程序支持以`CommonJS`规范组织代码结构
- */
-var wechat = require('./utils/wechat.js');
-
-/**
- * Douban API 模块
- * @type {Object}
- */
-var douban = require('./utils/douban.js');
-
-/**
- * Baidu API 模块
- * @type {Object}
- */
-var baidu = require('./utils/baidu.js');
-
+//app.js
 App({
-  /**
-   * Global shared
-   * 可以定义任何成员，用于在整个应用中共享
-   */
-  data: {
-    name: 'Douban Movie Review',
-    version: '0.1.0',
-    currentCity: '成都'
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
   },
-
-  /**
-   * WeChat API
-   */
-  wechat: wechat,
-
-  /**
-   * Douban API
-   */
-  douban: douban,
-
-  /**
-   * Baidu API
-   */
-  baidu: baidu,
-
-  /**
-   * 生命周期函数--监听小程序初始化
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
-   */
-  onLaunch: function onLaunch() {
-    var _this = this;
-
-    wechat.getLocation().then(function (res) {
-      var latitude = res.latitude,
-          longitude = res.longitude;
-
-      return baidu.getCityName(latitude, longitude);
-    }).then(function (name) {
-      _this.data.currentCity = name.replace('市', '');
-      console.log('currentCity : ' + _this.data.currentCity);
-    }).catch(function (err) {
-      _this.data.currentCity = '成都';
-      console.error(err);
-    });
+  globalData: {
+    userInfo: null
   }
-});
+})
